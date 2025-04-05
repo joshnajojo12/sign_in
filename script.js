80f55c6f440a1bb1b40f79ed507ce94151fe3c8c
joshnajojo12 <joshnajojo200621@gmail.com>
2025-04-05 16:00:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+function clickSubmitButton(event){
+    event.preventDefault();
+    const username=document.getElementById("username").value;
+    const welcomeMessage=document.getElementById('welcome-message');
+    welcomeMessage.textContent='welcome,'  + username + '!';
+    welcomeMessage.style.display='block';
+}
